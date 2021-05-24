da2ee5e099488d4e1ccec2524a01afb45db4b1bd
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:54:42 -0400

    ## 5.2.1
    ### Patch Changes
    
    - .gitignore: - \*.js

---
+++ src/_fb_html_attrs.js
@@ -0,0 +1,8 @@
+export function _fb_html_attrs() {
+    return {
+        xmlns: 'http://www.w3.org/1999/xhtml',
+        'xmlns:og': 'http://ogp.me/ns#',
+        'xmlns:fb': 'https://www.facebook.com/2008/fbml'
+    };
+}
+export { _fb_html_attrs as _attrs__html__fb };
