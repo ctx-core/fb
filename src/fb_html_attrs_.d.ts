74a380db37c70e7b75141ab62133a4a5d6cc8dfe
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 7.0.9
    ### Patch Changes
    
    - dist directory

--- src/fb_html_attrs_.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function fb_html_attrs_(): {
-    xmlns: string;
-    'xmlns:og': string;
-    'xmlns:fb': string;
-};
-export { fb_html_attrs_ as _fb_html_attrs, fb_html_attrs_ as _attrs__html__fb, };
--- src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './fb_html_attrs_';
